69d3132464c995e1c4894034c760597d8b1809e2
Michael Bottone <bottonedev@gmail.com>
2014-12-14 23:53:05 -0500

    Initial Commit

---
+++ rivalryapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rivalryapp
+//
+//  Created by Michael Bottone on 12/14/14.
+//  Copyright (c) 2014 Rivalry Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
